4f08cb18c353b3af276714a4ed2e1de93515eaf4
hai_do_488 <h.do488@mybvc.ca>
2019-03-09 11:51:24 -0700

    add click event when user cancel register acc to back to welcome page

--- ChatBerry/app/src/main/java/com/bowfletchers/chatberry/Register_account.java
+++ ChatBerry/app/src/main/java/com/bowfletchers/chatberry/Register_account.java
@@ -1,7 +1,9 @@
 package com.bowfletchers.chatberry;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Register_account extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class Register_account extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_account);
+    }
+
+    public void cancelRegister(View view) {
+        // navigate back to welcome page
+        Intent backToSignInIntent = new Intent(Register_account.this, WelcomePage.class);
+        startActivity(backToSignInIntent);
+    }
+
+    public void confirmRegister(View view) {
+        // navigate to the chat history page
     }
 }
