afe4d15905a31d951ac678b85faae2a6f6f39d5d
hai_do_488 <h.do488@mybvc.ca>
2019-03-12 12:54:28 -0600

    create Member class

--- ChatBerry/app/src/main/java/com/bowfletchers/chatberry/ClassLibrary/Member.java
+++ ChatBerry/app/src/main/java/com/bowfletchers/chatberry/ClassLibrary/Member.java
@@ -1,25 +1,49 @@
 package com.bowfletchers.chatberry.ClassLibrary;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Member {
     private String name;
     private final int profilePicture;
+    private List<Member> friendList;
+    private List<Chat> chatList;
+    private List<Story> stories;
 
 
     public Member (String name, int profilePicture) {
         this.name = name;
         this.profilePicture = profilePicture;
+        friendList = new ArrayList<>();
+        chatList = new ArrayList<>();
+        stories = new ArrayList<>();
     }
 
-
-    public String getName () {
+    public String getName() {
         return name;
     }
 
-
-    public int getProfilePicture () {
-        return this.profilePicture;
+    public int getProfilePicture() {
+        return profilePicture;
     }
 
+    public List<Member> getFriendList() {
+        return friendList;
+    }
 
+    public List<Chat> getChatList() {
+        return chatList;
+    }
 
+    public List<Story> getStories() {
+        return stories;
+    }
+
+    public void addFriend(Member friend){
+        this.friendList.add(friend);
+    }
+
+    public void removeFriend(Member friend) {
+        this.friendList.remove(friend);
+    }
 }
