138888be8ea283e4e0a3fa12990ae0fdbee08219
hai_do_488 <h.do488@mybvc.ca>
2019-04-11 17:00:26 -0600

    adding menu bar for friend list activity

--- ChatBerry/app/src/main/java/com/bowfletchers/chatberry/Activities/FriendList.java
+++ ChatBerry/app/src/main/java/com/bowfletchers/chatberry/Activities/FriendList.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.bowfletchers.chatberry.Adapters.FriendListAdapter;
@@ -23,10 +26,11 @@ public class FriendList extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_friend_list_ui);
+        setContentView(R.layout.activity_friend_list);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         Member testUser = MockData.buildUserObject();
-
         recyclerView = findViewById(R.id.recyclerview_friendlist);
         friendList = testUser.getFriendList();
         adapter = new FriendListAdapter(friendList);
@@ -37,5 +41,29 @@ public class FriendList extends AppCompatActivity {
     public void viewMessage(View view) {
         Intent goToIntent = new Intent(this, MessageViewer.class);
         startActivity(goToIntent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.my_profile:
+                Intent userProfileIntent = new Intent(FriendList.this, UserProfile.class);
+                startActivity(userProfileIntent);
+                return true;
+            case R.id.my_friends:
+                Intent userFriendsIntent = new Intent(FriendList.this, FriendList.class);
+                startActivity(userFriendsIntent);
+                return true;
+            case R.id.homePage:
+                Intent chatListIntent = new Intent(FriendList.this, ChatHistoryList.class);
+                startActivity(chatListIntent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
