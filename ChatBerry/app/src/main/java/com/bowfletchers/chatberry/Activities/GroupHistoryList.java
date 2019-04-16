b7e5c8e9440ae9e54588d077f8001a1af266aee2
hai_do_488 <h.do488@mybvc.ca>
2019-04-15 22:04:13 -0600

    add navbar for group list

--- ChatBerry/app/src/main/java/com/bowfletchers/chatberry/Activities/GroupHistoryList.java
+++ ChatBerry/app/src/main/java/com/bowfletchers/chatberry/Activities/GroupHistoryList.java
@@ -3,12 +3,16 @@ package com.bowfletchers.chatberry.Activities;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.bowfletchers.chatberry.Adapters.AvailableUsersInfoAdapter;
 import com.bowfletchers.chatberry.Adapters.GroupChatsListAdapter;
@@ -35,11 +39,35 @@ public class GroupHistoryList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_history_list);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        setTitle("Group chats");
+
         getYourGroups();
         mRecyclerView = findViewById(R.id.group_chats_list_recycler_view);
         mAdapter = new GroupChatsListAdapter(this, finalGroupsNameList, finalGroupIdList);
         mRecyclerView.setAdapter(mAdapter);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_only_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.homePageOnly:
+                Intent intent = new Intent(this, ChatHistoryList.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                this.startActivity(intent);
+                //Runtime.getRuntime().exit(0);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void getYourGroups() {
